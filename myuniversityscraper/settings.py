3c0850e63669ff2ff324e0e5e6ad3825891e35fe
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2014-02-10 04:01:53 +0200

    First commit of scrapy project skeleton.

---
+++ myuniversityscraper/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for myuniversityscraper project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'myuniversityscraper'
+
+SPIDER_MODULES = ['myuniversityscraper.spiders']
+NEWSPIDER_MODULE = 'myuniversityscraper.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'myuniversityscraper (+http://www.yourdomain.com)'

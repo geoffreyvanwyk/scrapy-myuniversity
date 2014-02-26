b01755502f04aa9eab91adc0f7d3cdc128bcfb01
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2014-02-26 20:08:04 +0200

    Refactored to stay within 79 chars/line limit.

--- myuniversityscraper/spiders/myuniversityscraper_spider.py
+++ myuniversityscraper/spiders/myuniversityscraper_spider.py
@@ -17,23 +17,27 @@ class MyuniversityscraperSpider(Spider):
 		return requests
 
 	def get_request(self, course_level, page_number):
-		return FormRequest('http://myuniversity.gov.au/' + course_level + 'Courses/GetSearchResults', formdata={
-			'courseCutOff': '100',
-			'courseCutOffQld': '1',
-			'courseEntryCutOffOption': '0',
-			'courseFee': '100000',
-			'courseFeeOption': '0',
-			'courseLevelOption': course_level,
-			'courseName': '',
-			'degreeStudyOption': '0',
-			'deliveryOption': '0',
-			'excludedWords': '',
-			'page':	str(page_number + 1),
-			'pageSize':	'Hundred',
-			'providerType':	'0',
-			'resetSelections': 'false',
-			'sortDirection': 'Ascending',
-			'sortOrder': ''
+		return FormRequest(
+			'http://myuniversity.gov.au/' +
+			course_level +
+			'Courses/GetSearchResults',
+			formdata={
+				'courseCutOff': '100',
+				'courseCutOffQld': '1',
+				'courseEntryCutOffOption': '0',
+				'courseFee': '100000',
+				'courseFeeOption': '0',
+				'courseLevelOption': course_level,
+				'courseName': '',
+				'degreeStudyOption': '0',
+				'deliveryOption': '0',
+				'excludedWords': '',
+				'page':	str(page_number + 1),
+				'pageSize':	'Hundred',
+				'providerType':	'0',
+				'resetSelections': 'false',
+				'sortDirection': 'Ascending',
+				'sortOrder': ''
 		}, callback=self.parse_page)
 
 	def parse_page(self, response):
@@ -41,34 +45,40 @@ class MyuniversityscraperSpider(Spider):
 		sel = Selector(response)
 
 		try:
-			course_elements = sel.xpath("//div[@class='myuni-small-cell-block']")
-			if course_elements == []:
-				raise WebPageChangedError('course_elements')
+			courses = sel.xpath("//div[@class='myuni-small-cell-block']")
+			if courses == []:
+				raise WebPageChangedError('courses')
 
-			for course_element in course_elements:
-				course_attribute_elements = course_element.xpath('.//span')
-				if course_attribute_elements == []:
-					raise WebPageChangedError('course_attribute_elements')
+			for course in courses:
+				fields = course.xpath('.//span')
+				if fields == []:
+					raise WebPageChangedError('fields')
 
-				course = Course()
-				course['course_name'] = course_attribute_elements[0].xpath('a/text()').extract()
+				crs = Course()
+				crs['course_name'] = fields[0].xpath('a/text()').extract()
+
 				if is_undergraduate:
-					course['cutoff_atar'] = course_attribute_elements[2].xpath('text()').extract()
-					course['duration'] = course_attribute_elements[4].xpath('text()').extract()
-					course['award_type'] = course_attribute_elements[5].xpath('text()').extract()
-					course['field_of_education'] = course_attribute_elements[6].xpath('text()').extract()
-					course['provider'] = course_attribute_elements[7].xpath('a/text()').extract()
-					course['campus'] = course_attribute_elements[8].xpath('text()').extract()
-					course['level'] = 'Undergraduate'
+					crs['cutoff_atar'] = fields[2].xpath('text()').extract()
+					crs['duration'] = fields[4].xpath('text()').extract()
+					crs['award_type'] = fields[5].xpath('text()').extract()
+					crs['field_of_education'] = (
+						fields[6].xpath('text()').extract()
+					)
+					crs['provider'] = fields[7].xpath('a/text()').extract()
+					crs['campus'] = fields[8].xpath('text()').extract()
+					crs['level'] = 'Undergraduate'
 				else:
-					course['cutoff_atar'] = ''
-					course['duration'] = course_attribute_elements[2].xpath('text()').extract()
-					course['award_type'] = course_attribute_elements[3].xpath('text()').extract()
-					course['field_of_education'] = course_attribute_elements[4].xpath('text()').extract()
-					course['provider'] = course_attribute_elements[5].xpath('a/text()').extract()
-					course['campus'] = course_attribute_elements[6].xpath('text()').extract()
-					course['level'] = 'Postgraduate'
-				yield course
+					crs['cutoff_atar'] = ''
+					crs['duration'] = fields[2].xpath('text()').extract()
+					crs['award_type'] = fields[3].xpath('text()').extract()
+					crs['field_of_education'] = (
+						fields[4].xpath('text()').extract()
+					)
+					crs['provider'] = fields[5].xpath('a/text()').extract()
+					crs['campus'] = fields[6].xpath('text()').extract()
+					crs['level'] = 'Postgraduate'
+
+				yield crs
 
 			yield self.get_next_page(sel, is_undergraduate)
 
@@ -82,16 +92,29 @@ class MyuniversityscraperSpider(Spider):
 	def get_next_page(self, sel, is_undergraduate):
 		try:
 			paginator = sel.xpath(
-				"//div[@class='myuni-alignright-whenbig'][../p[@id='navigationDescriptor']]/label"
+				"//div[@class='myuni-alignright-whenbig']" +
+				"[../p[@id='navigationDescriptor']]/label"
 			)
 			if paginator == []:
 				raise WebPageChangedError('paginator')
 
-			number_of_pages = int(paginator.xpath("pan[last()]/text()").extract()[0].replace('of', ''))
+			number_of_pages = (
+				int(
+					paginator
+						.xpath("span[last()]/text()")
+						.extract()[0]
+						.replace('of', '')
+				)
+			)
+
 			current_page = int(paginator.xpath("input/@value").extract()[0])
 
 			if number_of_pages > current_page:
-				course_level = 'Undergraduate' if is_undergraduate else 'Postgraduate'
+				if is_undergraduate:
+					course_level = 'Undergraduate'
+				else:
+					course_level = 'Postgraduate'
+
 				return self.get_request(course_level, current_page)
 
 		except IndexError:

3f06c75e7ed2877651a371f6d4cce509fac35768
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2014-02-26 21:12:49 +0200

    Creates results director if it does not exist.

--- myuniversityscraper_run.py
+++ myuniversityscraper_run.py
@@ -1,5 +1,8 @@
-from os import system
+from os import mkdir, path, system
 from datetime import datetime
+
+if not path.isdir('results'):
+	mkdir('results', 0755)
 
 timestamp = datetime.today().strftime("%Y-%m-%dT%H-%M-%S")
 filename = 'results/myuniversity-courses-' + timestamp + '.csv'
